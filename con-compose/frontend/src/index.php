76186fc1e370fcd4cbe4c560cd129a2b6b9eb536
Roger Andrés Venegas Opazo <127449516+rogandven@users.noreply.github.com>
2026-06-01 04:42:54 -0400

    update

--- con-compose/frontend/src/index.php
+++ con-compose/frontend/src/index.php
@@ -1,3 +1,31 @@
 <?php
     phpinfo();
+
+    try {
+        $serverName = "yourServername";
+        $connectionOptions = array(
+            "database" => "yourDatabase",
+            "uid" => "yourUsername",
+            "pwd" => "yourPassword"
+        );
+
+        // Establishes the connection
+        $conn = sqlsrv_connect($serverName, $connectionOptions);
+        if ($conn === false) {
+            throw new Exception((formatErrors(sqlsrv_errors())));
+        }
+
+        // Select Query
+        $tsql = "SELECT @@Version AS SQL_VERSION";
+
+        // Executes the query
+        $stmt = sqlsrv_query($conn, $tsql);
+
+        // Error handling
+        if ($stmt === false) {
+            throw new Exception(formatErrors(sqlsrv_errors()));
+        }
+    } catch (Exception $e) {
+        echo $e;
+    }
 ?>
